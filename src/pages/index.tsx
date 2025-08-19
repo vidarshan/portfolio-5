895b9fc0ccf0da026e67b1273125053e8ceb2cb3
vidarshan <vidarshanadithya3@gmail.com>
2025-08-19 11:21:28 -0700

    Added framework

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
+import "@mantine/core/styles.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import {
+  Box,
+  Container,
+  createTheme,
+  Grid,
+  MantineProvider,
+  Text,
+  ThemeIcon,
+  Title,
+} from "@mantine/core";
+import { SiGithub, SiLinkedin, SiStackoverflow } from "react-icons/si";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +26,24 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  const theme = createTheme({
+    colors: {
+      // or replace default theme color
+      dark: [
+        "#ffffff",
+        "#78e110",
+        "#8600b3",
+        "#0021c8",
+        "#d4df08",
+        "#14f814",
+        "#d77e02",
+        "#000000",
+        "#79ea00",
+        "#d80073",
+      ],
+    },
+  });
+
   return (
     <>
       <Head>
@@ -22,96 +52,60 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
-        <main className={styles.main}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol>
-            <li>
-              Get started by editing <code>src/pages/index.tsx</code>.
-            </li>
-            <li>Save and see your changes instantly.</li>
-          </ol>
 
-          <div className={styles.ctas}>
-            <a
-              className={styles.primary}
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
+      <MantineProvider defaultColorScheme="dark" theme={theme}>
+        <Container size="xl">
+          <Grid>
+            <Grid.Col
+              span={{ xs: 6, sm: 6, md: 6, lg: 3, xl: 3 }}
+              h="100vh"
+              component="header"
             >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.secondary}
+              <Title fw={500} mt="2rem" order={1}>
+                Vidarshan
+              </Title>
+              <Title size={20} fw={400} order={2}>
+                Software Engineer — Web, Mobile, AI & Cloud
+              </Title>
+              <Box mt="3rem" component="nav">
+                <Box component="ul">
+                  <Box component="li">
+                    <Text size="xl" component="a" href="#">
+                      About
+                    </Text>
+                  </Box>
+                  <Box component="li">
+                    <Text component="a" href="#">
+                      Projects
+                    </Text>
+                  </Box>
+                  <Box component="li">
+                    <Text component="a" href="#">
+                      Contact
+                    </Text>
+                  </Box>
+                </Box>
+              </Box>
+              <Box mt="3rem">
+                <SiGithub size={20} style={{ marginRight: "6px" }} />
+
+                <SiStackoverflow size={20} style={{ marginRight: "6px" }} />
+
+                <SiLinkedin size={20} style={{ marginRight: "6px" }} />
+              </Box>
+            </Grid.Col>
+
+            <Grid.Col
+              span={{ xs: 6, sm: 6, md: 6, lg: 9, xl: 9 }}
+              component="main"
             >
-              Read our docs
-            </a>
-          </div>
-        </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            Learn
-          </a>
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/window.svg"
-              alt="Window icon"
-              width={16}
-              height={16}
-            />
-            Examples
-          </a>
-          <a
-            href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              aria-hidden
-              src="/globe.svg"
-              alt="Globe icon"
-              width={16}
-              height={16}
-            />
-            Go to nextjs.org →
-          </a>
-        </footer>
-      </div>
+              content goes here
+            </Grid.Col>
+          </Grid>
+        </Container>
+
+        <footer className={styles.footer}></footer>
+      </MantineProvider>
     </>
   );
 }
